c25586427f8cdbce8178761c86d02a450bd4e1a0
Farwa626 <farwaafzaal70@gmail.com>
2025-09-10 16:50:40 -0800

    Add files via upload

--- rentproperty.php
+++ rentproperty.php
@@ -89,62 +89,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </style>
 </head>
 <body>
-    <header class="header">
-        <nav class="navbar nav-1">
-            <section class="flex">
-                <a href="home.php" class="logo"><i class="fas fa-house"></i>Elite Estate</a>
-                <ul>
-                    <li><a href="view property/post property.html">Post Property<i class="fas fa-paper-plane"></i></a></li>
-                </ul>
-            </section>
-        </nav>
-        
-        <nav class="navbar nav-2">
-            <section class="flex">
-                <div id="menu-btn" class="fas fa-bars"></div>
-                <div class="menu">
-                    <ul>
-                        <li><a href="#">Buy<i class="fas fa-angle-down"></i></a>
-                            <ul>
-                                <li><a href="list2.html">House</a></li>
-                                <li><a href="list3.html">Flat</a></li>
-                                <li><a href="furnish1.html">Furnished</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Sell<i class="fas fa-angle-down"></i></a>
-                            <ul>
-                                <li><a href="pview property/post property.html">Post property</a></li>
-                                <li><a href="dash.html">Dashboard</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Rent</a>
-                            <ul>
-                                <li><a href="house.html">House</a></li>
-                                <li><a href="flats.html">Flat</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Help<i class="fas fa-angle-down"></i></a>
-                            <ul>
-                                <li><a href="about1.html">About Us</a></i></li>
-                                <li><a href="contact.html">Contact Us</a></i></li>
-                                <li><a href="contact.html#faq">FAQ</a></i></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-                
-                <ul>
-                    <li><a href="saved.html">Saved <i class="far fa-heart"></i></a></li>
-                    <li><a href="#">Account <i class="fas fa-angle-down"></i></a>
-                        <ul>
-                            <li><a href="login.php">Login</a></li>
-                            <li><a href="register.html">Register</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </section>
-        </nav>
-    </header>
+<?php include 'header.php' ?>
 
     <div class="form-container">
         <h2>Book This Property</h2>
@@ -178,32 +123,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </form>
     </div>
 
-    <footer class="footer">
-        <section class="flex">
-            <div class="box">
-                <a href="tel:1234567890"><i class="fas fa-phone"></i><span>123456789</span></a>
-                <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
-                <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope"></i><span>hajrahamad@gmail.com</span></a>
-                <a href="#"><i class="fas fa-map-marker-alt"></i><span>Pakistan, Mandi bhauddin - 50400</span></a>
-            </div>
-            <div class="box">
-                <a href="home.php" class="a"><span>Home</span></a>
-                <a href="about1.html" class="b"><span>About</span></a>
-                <a href="contact.html" class="c"><span>Contact</span></a>
-                <a href="list2.html" class="d"><span>All listings</span></a>
-                <a href="saved.html" class="e"><span>Saved properties</span></a>
-            </div>
-            <div class="box">
-                <a href="https://www.facebook.com/profile.php?id=61577889561897"><i class="fab fa-facebook-f"></i><span>&nbsp;&nbsp;Facebook</span></a>
-                <a href="https://twitter-cl.vercel.app/home"><i class="fab fa-twitter"></i><span>&nbsp;&nbsp;Twitter</span></a>
-                <a href="https://www.linkedin.com/in/elite-estate-314823370/"><i class="fab fa-linkedin"></i><span>&nbsp;&nbsp;Linkedin</span></a>
-                <a href="https://www.instagram.com/elite_state23/?hl=en"><i class="fab fa-instagram"></i><span>&nbsp;&nbsp;Instagram</span></a>
-            </div>
-        </section>
-        <div class="credit">&copy; copyright @ 2024-2025 by <span>ELITE ESTATE</span> | all rights reserved!</div>
-    </footer>
-
-    <script src="js/script.js"></script>
+<?php include 'footer.php' ?>
 
 </body>
 </html>
